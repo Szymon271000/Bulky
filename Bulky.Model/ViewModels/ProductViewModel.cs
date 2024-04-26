16436073dd473dd5421f5e960eedb19a86e32543
Szymon271000 <deborah.fazzuoli@istitutopantheon.it>
2024-04-26 16:12:10 +0200

    Add image URL by creating product

--- Bulky.Model/ViewModels/ProductViewModel.cs
+++ Bulky.Model/ViewModels/ProductViewModel.cs
@@ -11,6 +11,7 @@ namespace Bulky.Models.ViewModels
     public class ProductViewModel
     {
         public Product Product { get; set; }
+        [ValidateNever]
         public IEnumerable<SelectListItem> CategoryList { get; set; }
     }
 }
--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -12,9 +12,12 @@ namespace BulkyWeb.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        public ProductController(IUnitOfWork unitOfWork)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
+
         }
         public IActionResult Index()
         {
@@ -54,6 +57,19 @@ namespace BulkyWeb.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
+                string wwwRootPath = _webHostEnvironment.WebRootPath;
+                if (file != null)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string productPath = Path.Combine(wwwRootPath, @"images\product");
+
+                    using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+
+                    productToAdd.Product.ImageUrl = @"\images\product\" + fileName;
+                }
                 _unitOfWork.productRepository.Add(productToAdd.Product);
                 _unitOfWork.Save();
                 TempData["success"] = "Category created successfully";
