7f1b362061f073cb9bd2b7cf72478b9267b7d56f
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2024-06-29 15:41:17 +0200

    Added status filter and create details method in controller

--- BulkyWeb/Areas/Admin/Controllers/OrderController.cs
+++ BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -1,7 +1,10 @@
 ﻿using Bulky.DataAccess.Repository.IRepository;
 using Bulky.Models;
+using Bulky.Models.ViewModels;
+using Bulky.Utility;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace BulkyWeb.Areas.Admin.Controllers
 {
@@ -18,12 +21,38 @@ namespace BulkyWeb.Areas.Admin.Controllers
             return View();
         }
 
-
+        public IActionResult Details(int id)
+        {
+            OrderVm orderVm= new()
+            {
+                oderHeader = _unitOfWork.orderHeaderRepository.Get(x => x.Id == id, includeProperties: "ApplicationUser"),
+                orderDetail = _unitOfWork.orderDetailRepository.GetAll(x => x.OrderHeaderId == id, includeProperties: "Product")
+            };
+            return View(orderVm);
+        }
         #region API CALLS
         [HttpGet]
-        public IActionResult GetAll()
+        public IActionResult GetAll(string status)
         {
-            IList<OrderHeader> objOrderHeaders = _unitOfWork.orderHeaderRepository.GetAll(includeProperties: "ApplicationUser").ToList();
+            IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.orderHeaderRepository.GetAll(includeProperties: "ApplicationUser").ToList();
+
+            switch (status)
+            {
+                case "pending":
+                    objOrderHeaders = objOrderHeaders.Where(x => x.PaymentStatus == SD.StatusPending);
+                    break;
+                case "inprocess":
+                    objOrderHeaders = objOrderHeaders.Where(x => x.PaymentStatus == SD.StatusInProgress);
+                    break;
+                case "completed":
+                    objOrderHeaders = objOrderHeaders.Where(x => x.PaymentStatus == SD.StatusShipped);
+                    break;
+                case "approved":
+                    objOrderHeaders = objOrderHeaders.Where(x => x.PaymentStatus == SD.StatusApproved || x.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                    break;
+                default:
+                    break;
+            }
             return Json(new { data = objOrderHeaders });
         }
         #endregion
